f4abc4f6d15450393712e74091efb34d03ddeed8
James Norwood <norwood_18@hotmail.com>
2016-12-07 12:20:03 -0700

    Add files via upload
    uploaded current .py project and screenshot of recipe.

---
+++ ldf_data_element-2016.py
@@ -0,0 +1,47 @@
+#define magics network ip
+magicsIP = '\\172.16.69.105\magics\PMON S
+
+#define location of data element text file
+dataElementText = 'G:\MT2_DataElement'
+
+
+
+#define dictionaries required for recipe names.
+recipeName = ['AR8-ARC' : '2010',
+              'Cr PMON AOJ' : 'test00',
+              'Cr PMON Pre' : 'test01',
+              'Cr PMON Pst' : 'test02',
+              'FEN ARC' : 'test03',
+              'FEN NTAR7' : 'test04',
+              'FEP ARC' : 'test05',
+              'LMON AWI' : 'test06',
+              'LMON Cr' : 'test07',
+              'LMON Cr S&S' : 'test08',
+              'LMON Eng' : 'test09',
+              'LMON Pst Cr Etch' : 'test10',
+              'LMON Pst Cr Etch GenIV' : 'test11',
+              'LMON Pst Cr Etch OMOG' : 'test12',
+              'LMON Pst Cr Etch S&S' : 'test13',
+              'LMON Pst Dev OMOG' : 'test14',
+              'LMON Pst Develop' : 'test15',
+              'LMON Pst Mosi Etch' : 'test16',
+              'LMON Pst Mosi Etch S&S' : 'test17',
+              'MBX Pst Dev LSMON' : 'test18',
+              'MBX Pst Strip' : 'test19',
+              'NTAR5 ACC' : 'test20',
+              'NTAR5 Pre' : 'test21',
+              'NTAR7 ACC' : 'test22',
+              'NTAR7 Pre' : 'test23',
+              'OMOG ACC' : 'test24',
+              'NTAR5 ACC' : 'test20',
+              'NTAR5 ACC' : 'test20',
+              
+              
+
+
+              'MBX Pst Dev LSMON' : 'test18',
+
+
+
+              
+              
